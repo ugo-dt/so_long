40d7168d2fd8f4e213f4cadc76e8c8f9b7c2d99e
Ugo DANIEL-THOMAS <ugdaniel@student.42.fr>
2021-06-22 18:39:41 +0200

    script to play all maps

---
+++ play_maps.sh
@@ -0,0 +1,10 @@
+make
+echo "Loading map 0..."
+./so_long maps/map0.ber
+echo "Loading map 1..."
+./so_long maps/map1.ber
+echo "Loading map 2..."
+./so_long maps/map2.ber
+echo "Loading map 3..."
+./so_long maps/map3.ber
+echo "Game over!"
